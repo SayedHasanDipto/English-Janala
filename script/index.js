fa1a6e8c541d875f95a097be6cff28c0dfe08812
SayedHasanDipto <sayedhasandipto@GMAIL.COM>
2026-03-05 01:40:45 +0600

    Adding active buttons

--- script/index.js
+++ script/index.js
@@ -4,39 +4,40 @@ const loadLessons = () => {
         .then((res) => res.json())
         .then((data) => displayLessons(data.data));
 }
-
+const removeActive = () => {
+    const activeButtons = document.querySelectorAll('.active-btn');
+    // console.log(activeButtons);
+    activeButtons.forEach(activeButton => activeButton.classList.remove('btn-active'));
+}
 const levelByWord = (id) => {
     // console.log(url);
     const url = `https://openapi.programming-hero.com/api/level/${id}`;
     fetch(url)
         .then(res => res.json())
-        .then(wordData => displayWordLesson(wordData.data));
-}
+        .then(wordData => {
+            removeActive();
+            const clickBtn = document.getElementById(`lesson-btn-${id}`);
 
+            clickBtn.classList.add('btn-active');
+
+            displayWordLesson(wordData.data)
+        });
+}
 const displayWordLesson = (datas) => {
     const wordContainer = document.getElementById('word-container');
     wordContainer.innerHTML = "";
 
     if (datas.length == 0) {
         wordContainer.innerHTML = `
-        
-
-        
-        
-
         <div class="text-center col-span-full py-16 space-y-6 rounded-xl">
             <img src="./assets/alert-error.png" alt="aleartError" class='mx-auto'>
             <p class="font-bangla leading-6  mb-3 text-[#79716B]">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
             <h1 class="font-bangla leading-10 text-4xl text-[#292524]">নেক্সট Lesson এ যান</h1>
         </div>
-        
-        
         `;
         return;
 
     }
-
-
     datas.forEach(data => {
         // console.log(data);
         const card = document.createElement('div');
@@ -46,7 +47,7 @@ const displayWordLesson = (datas) => {
             <p class="font-semibold text-xl">Meaning /Pronounciation</p>
 
             <div class="font-bangla font-semibold text-4xl text-[#18181bb0]">
-                "${data.meaning ? data.meaning:'অর্থ পাওয়া যাইনি'} / ${data.pronunciation ? data.pronunciation: 'উচ্চারণ পাওয়া যাইনি'}"
+                "${data.meaning ? data.meaning : 'অর্থ পাওয়া যাইনি'} / ${data.pronunciation ? data.pronunciation : 'উচ্চারণ পাওয়া যাইনি'}"
             </div>
 
             <div class="flex justify-between items-center">
@@ -55,33 +56,25 @@ const displayWordLesson = (datas) => {
                 </button>
                 <button class="btn btn-soft btn-primary">
                     <i class="fa-solid fa-volume-high"></i>
-                </button>
-                
-
+                </button>           
             </div>
         </div>
         `;
-
         wordContainer.append(card);
     });
 }
-
 const displayLessons = (lessons) => {
-
     // 1. Get the container
-
     const levelContainer = document.getElementById('level-container')
     levelContainer.innerHTML = '';
-
     // 2.  Get into every lesson
-
     for (const lesson of lessons) {
         const btnDiv = document.createElement('div');
         btnDiv.innerHTML = `
-        <button onclick="levelByWord(${lesson.level_no})" class="btn btn-outline btn-primary">
-            <i class="fa-solid fa-book-open"></i>
-            ${lesson.lessonName}
-        </button>
+                         <button id="lesson-btn-${lesson.level_no}" onclick="levelByWord(${lesson.level_no})" class="btn btn-outline btn-primary active-btn">
+                                <i class="fa-solid fa-book-open"></i>
+                                ${lesson.lessonName}
+                         </button>
     `;
         levelContainer.append(btnDiv);
     }
